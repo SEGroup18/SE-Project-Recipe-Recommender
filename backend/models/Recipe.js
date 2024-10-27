d3a6d8ea884d54231bddd3ba32f992b07a04b124
swaraj kaondal <skaonda@ncsu.edu>
2024-10-27 16:00:36 -0400

    Seeded data using the data_loader script

--- backend/models/Recipe.js
+++ backend/models/Recipe.js
@@ -1,42 +1,69 @@
 const mongoose = require("mongoose");
 
-const recipeSchema = mongoose.Schema(
-  {
-    name: {
+const recipeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  minutes: {
+    type: Number,
+  },
+  tags: [
+    {
       type: String,
       trim: true,
-      required: true,
-      unique: true,
     },
-    description: {
+  ],
+  steps: [
+    {
       type: String,
       trim: true,
     },
-    minutes: {
+  ],
+  ingredients: [
+    {
+      type: String,
+      trim: true,
+    },
+  ],
+  nutrients: {
+    calories: {
       type: Number,
     },
-    tags: [
-      {
-        type: String,
-        trim: true,
-      },
-    ],
-    steps: [
-      {
-        type: String,
-        trim: true,
-      },
-    ],
-    ingredients: [
-      {
-        type: String,
-        trim: true,
-      },
-    ],
+    protein: {
+      type: Number,
+    },
+    fat: {
+      type: Number,
+    },
+    sugar: {
+      type: Number,
+    },
+    sodium: {
+      type: Number,
+    },
+    iron: {
+      type: Number,
+    },
+    vitamin_c: {
+      type: Number,
+    },
+    cholesterol: {
+      type: Number,
+    },
+    trans_fat: {
+      type: Number,
+    },
+    calcium: {
+      type: Number,
+    },
   },
-  {
-    timestamps: true,
-  }
-);
+});
 
 module.exports = mongoose.model("Recipe", recipeSchema);
