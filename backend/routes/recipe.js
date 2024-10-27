100d733f081e346aba4eeb0b277fbcf8221904e9
swaraj kaondal <skaonda@ncsu.edu>
2024-10-27 17:59:35 -0400

    Added Ingredient Search page

--- backend/routes/recipe.js
+++ backend/routes/recipe.js
@@ -6,11 +6,15 @@ const {
   getRecipe,
   updateRecipe,
   deleteRecipe,
+  getIngredients,
+  getRecipesByIngredient,
 } = require("../controllers/recipe");
 const router = express.Router();
 
 router.post("/add", createRecipe);
 router.get("/all", getAllRecipes);
+router.post("/recipeByIngredient", getRecipesByIngredient);
+router.get("/ingredients", getIngredients);
 router.get("/some", getSomeRecipes);
 router.get("/:recipeID", getRecipe);
 router.patch("/:recipeID", updateRecipe);
