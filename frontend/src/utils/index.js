a8b05d06b68caea7fda843ecb45269fcaa7798a6
swaraj kaondal <skaonda@ncsu.edu>
2024-10-31 12:50:58 -0400

    pushing changes to host the app

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 // const server = axios.create({ baseURL: "http://ec2-3-238-67-17.compute-1.amazonaws.com:5000/" });
-const server = axios.create({ baseURL: "http://localhost:5000/" });
+const server = axios.create({
+  baseURL: "se-project-recipe-recommender-production-6299.up.railway.app",
+});
 
 export { server };
