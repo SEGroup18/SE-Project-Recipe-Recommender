6d243d26313958a48ccfb6d461e071f1e463310a
Aniruddha Shivananda <aniruddhams2023@gmail.com>
2024-11-26 18:24:49 -0500

    bug fix

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // const server = axios.create({ baseURL: "http://ec2-3-238-67-17.compute-1.amazonaws.com:5000/" });
 const server = axios.create({
-  baseURL: "http://localhost:5000/",
+  baseURL: "http://localhost:5001/",
 });
 
 export { server };
